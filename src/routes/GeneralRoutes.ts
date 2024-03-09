5f88622dc81a9c98320e271ec376cd782fe8be63
phgodoycosta <phgodoycosta@gmail.com>
2024-03-08 21:23:04 -0400

    Adicionando página principal

--- src/routes/GeneralRoutes.ts
+++ src/routes/GeneralRoutes.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 const router = Router()
 
 router.get("/", (request: Request, response: Response) => {
-    
+    return response.render("pages/index")
 })
 
 export default router
--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,12 @@ import cors from 'cors'
 const app = express()
 
 app.set("view engine", "pug")
+app.set("views", path.join(__dirname, "/views"))
 
 app.use(GeneralRouter)
 app.use(cors({origin: "*"}))
-app.use(express.static(path.join(__dirname, "../public")))
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "/public")))
 
 
 app.listen(3000, () => {
